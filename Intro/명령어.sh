04d324d8f9ea431dbb739e9fff3099e3deea6b46
pdy1207 <pdyme1207@gmail.com>
2023-10-04 21:53:51 +0900

    🦆 feat: [23.10.04] 설치 후 셋팅 및 순정 상태 백업 ssh server설치 장치 클립보드 / 드래그 앤 드롭 양방향 VM 에서의 ssh 설치

--- Intro/명령어.sh
+++ Intro/명령어.sh
@@ -11,6 +11,15 @@
 
 `apt install ssh` : 해당 ssh설치
 
+`ssh` : 해당 쉘 명령어 종류? 
+
 `ps -ef | grep ssh` : 프로세스 확인
 
 그리고 putty로서 127.0.0.1 연결되는지확인!
+
+
+---
+
+`sudo apt -y install ssh` : ssh 설치
+
+`sudo systemctl enable ssh`: putty 에서의 ssh 재부팅에도 자동으로 설정
